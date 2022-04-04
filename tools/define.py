4451b02bd450e8bd66e6fea32e62e5a060f0f8e5
SapoKR <phmi1621@naver.com>
2022-04-04 19:32:55 +0900

    append define code

---
+++ tools/define.py
@@ -0,0 +1,7 @@
+from discord.ext import commands
+import discord
+from typing import Union
+from tools.db import 
+async def check_User(user: Union[discord.Member, discord.User):
+    
+async def isinUserData(user: Union[discord.Member, discord.User]):
