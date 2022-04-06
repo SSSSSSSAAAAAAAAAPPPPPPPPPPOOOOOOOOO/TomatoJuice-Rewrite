46dc764df10ff3a397a6e651902aed246f69d643
SapoKR <phmi1621@naver.com>
2022-04-06 22:29:29 +0900

    from_rgb error fixed(maybe)

--- tools/bot.py
+++ tools/bot.py
@@ -27,38 +27,41 @@ class Juice(commands.Bot):
                 run_task=True,
             )
 
-
-        self.color = discord.Color.from_rgb(colortmp[0], colortmp[1], colortmp[2]),
+        self.color = '#{:X}{:X}{:X}'.format(colortmp[0], colortmp[1], colortmp[2]),
 
     def request_permission(self, permission_name: str):
-        embed = discord.Embed(title="길드에 권한이 없는거 같아요!", description=f"{permission_name} 권한이 없어서 명령어를 실행할수 없어요!", color=self.color)
+        embed = discord.Embed(title="길드에 권한이 없는거 같아요!", description=f"{permission_name} 권한이 없어서 명령어를 실행할수 없어요!",
+                              color=self.color)
         return embed
 
     async def on_message(self, message):
         if not await check_User(message.author):
             embed = discord.Embed(
-               title=f'{self.user.name}를 사용하시려면 아래의 약관에 동의를 하셔야해요!',
-               description='봇 명령어의 내용이 일부 수집될수 있습니다.\n사용자의 아이디가 유저 DB에 등록됩니다.',
-               color=self.color
+                title=f'{self.user.name}를 사용하시려면 아래의 약관에 동의를 하셔야해요!',
+                description='봇 명령어의 내용이 일부 수집될수 있습니다.\n사용자의 아이디가 유저 DB에 등록됩니다.',
+                color=self.color
             )
-            view = selectview(message.author, [await load_text(message.author, 'yes'), await load_text(message.author, 'no')])
+            view = selectview(message.author,
+                              [await load_text(message.author, 'yes'), await load_text(message.author, 'no')])
             msg = await message.channel.send(embed=embed, view=view)
             await view.wait()
             if view.value == 'yes':
-               post = {'_id': message.author.id,
-                       'money': 0,
-                       'other': {},
-                       'locate': 'en_US',
-                       }
-               await D_users.insert_one(post)
-               processed = await load_text(message.author, 'registeruser')
-               await message.channel.send(embed=discord.Embed(title='Please change language!', description='Your Language is Now en_Us\nPlease change your country language', color=self.color))
+                post = {'_id': message.author.id,
+                        'money': 0,
+                        'other': {},
+                        'locate': 'en_US',
+                        }
+                await D_users.insert_one(post)
+                processed = await load_text(message.author, 'registeruser')
+                await message.channel.send(embed=discord.Embed(title='Please change language!',
+                                                               description='Your Language is Now en_Us\nPlease change your country language',
+                                                               color=self.color))
 
             elif view.value == 'no':
-               processed = await load_text(message.author, 'm_cancel')
+                processed = await load_text(message.author, 'm_cancel')
 
             else:
-               processed = await load_text(message.author, 'm_timeout')
+                processed = await load_text(message.author, 'm_timeout')
 
             await msg.edit(content=processed, embed=None, view=None)
 
@@ -75,5 +78,4 @@ class Juice(commands.Bot):
             return
 
         else:
-            await ctx.send((await load_text(ctx.author, 'Unable_Error'))+f'```{error}```')
-
+            await ctx.send((await load_text(ctx.author, 'Unable_Error')) + f'```{error}```')
