18ae64d9d48e8b64039b1a93947157cdcf2f27d5
SapoKR <phmi1621@naver.com>
2022-04-07 16:16:28 +0900

    something wrong.. fixed!

--- autoregister.py
+++ autoregister.py
@@ -16,16 +16,13 @@ async def main(data, locate_file):
             await lang.update_one({'_id': i}, {'$set': tmp})
 
 
-if __init__ == "__main__":
-
-    locate_file = input('>')
-
-    try:
-        with open('language' + locate_file + '.json', 'r') as f:
-            data = json.load(f)
-    except Exception as e:
-        print(e)
-        sys.exit(-1)
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main(data, locate_file))
-    loop.close()
+locate_file = input('>')
+try:
+    with open('language' + locate_file + '.json', 'r') as f:
+        data = json.load(f)
+except Exception as e:
+    print(e)
+    sys.exit(-1)
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main(data, locate_file))
+loop.close()
